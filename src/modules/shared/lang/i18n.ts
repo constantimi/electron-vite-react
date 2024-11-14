6e061afe8435d5aecc386c9f92e511142cb812ca
Konstantin Mihaylov <konstantin.mihaylov.io@gmail.com>
2024-11-14 23:58:07 +0100

    Implement the build workflow

--- src/modules/shared/lang/i18n.ts
+++ src/modules/shared/lang/i18n.ts
@@ -2,13 +2,18 @@ import i18n from 'i18next';
 import Backend from 'i18next-http-backend';
 import sprintf from 'i18next-sprintf-postprocessor';
 import { initReactI18next } from 'react-i18next';
+import { LanguageMetadata } from './types';
 import { en, enMetadata } from './locales/en';
-import { nl } from './locales/nl';
+import { nl, nlMetadata } from './locales/nl';
+
+export const languages: LanguageMetadata[] = [enMetadata, nlMetadata];
 
 const resources = {
   en,
   nl,
 };
+
+const savedLanguage = localStorage.getItem('lang') || enMetadata.code;
 
 i18n
   .use(initReactI18next)
@@ -16,10 +21,14 @@ i18n
   .use(sprintf)
   .init({
     resources,
-    lng: enMetadata.code,
+    lng: savedLanguage,
     fallbackLng: enMetadata.code,
     interpolation: {
       escapeValue: false,
     },
     debug: false,
   });
+
+i18n.on('languageChanged', (lng) => {
+  localStorage.setItem('lang', lng);
+});
